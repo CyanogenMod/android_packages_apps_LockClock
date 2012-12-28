64090cd997e4f6a9b3f02f5ba96e32e297f6403e
DvTonder <david.vantonder@gmail.com>
2012-12-27 23:15:20 -0500

    Initial commit

---
+++ src/com/cyanogenmod/lockclock/misc/Constants.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * * Licensed under the GNU GPLv2 license
+ *
+ * The text of the license can be found in the LICENSE file
+ * or at https://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+package com.cyanogenmod.lockclock.misc;
+
+public class Constants {
+
+    public static final String UPDATE_CHECK_PREF = "pref_update_check";
+    public static final String LAST_UPDATE_CHECK_PREF = "pref_last_update_check";
+
+    // Activity start parameters
+    public static final String CHECK_FOR_UPDATE = "check_for_update";
+    public static final String MANUAL_UPDATE = "manual_update";
+
+    // Update Check items
+    public static final int UPDATE_FREQ_MANUAL  = 0;
+    public static final int UPDATE_FREQ_DEFAULT = 10; // Should be 60
+
+}
