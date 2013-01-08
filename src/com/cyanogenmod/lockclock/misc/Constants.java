7d38ec0815574963fcc4476326b71b748e60039b
DvTonder <david.vantonder@gmail.com>
2013-01-08 16:25:09 -0500

    Chronus: Optimize widget loading/display
    Completely refactors the Calendar handling and changes the refresh
    interval from every minute to the minimum of either the next weather
    refresh or the next event start/end.
    
    It also now detects more events (new calendar entry, deleted calendar
    entry, location change, timezone change etc)
    
    Patch set 7 : Change the startup of the provider to only update the
                  required panels on enabling the appwigdet and updates
                  This further reduces the number of times the widget
                  refreshes itself, now limited only to actual events
    Patch set 8 : Change the weather refresh time to be absolute, not
                  relative to current time
    Patch set 9 : Refactor the calendar to use a CalendarInfo class that
                  maintains a static list of events.  This allows the
                  refreshing of the lock screen widget without querying
                  the calendar provider every time the screen turns on
    Patch set 10: Simplified onReceive loading/updating of widget with
                  additional tweaks to handling deleted widgets and
                  a fix to the refresh timer calculations including a
                  check of the lookahead window.
    Patch set 11: Change the alarmservice to wake the CPU and do the
                  update if the device is sleeping.  This way, since the
                  updates are so infrequent now, it makes sense to ensure
                  things are updated (if needed) when the user turns the
                  screen on after a while.
    
    Change-Id: I0496dad356c92fb26ad7289268327b27b365b6cd

--- src/com/cyanogenmod/lockclock/misc/Constants.java
+++ src/com/cyanogenmod/lockclock/misc/Constants.java
@@ -21,7 +21,9 @@ public class Constants {
     public static final String PREFERENCES_CHANGED = "preferences_changed";
 
     // Activity start commands
-    public static final String FORCE_REFRESH = "force_refresh";
+    public static final String REFRESH_WEATHER = "refresh_weather";
+    public static final String REFRESH_CLOCK = "refresh_clock";
+    public static final String REFRESH_CALENDAR = "refresh_calendar";
 
     // Widget Settings
     public static final String CLOCK_DIGITAL = "clock_digital";
