4aa4911fc16bfaf2ea034c9f83a65b16e726fd85
DvTonder <david.vantonder@gmail.com>
2013-01-08 16:25:09 -0500

    Chronus: Optimize widget loading/display
    Completely refactors the Calendar handling and changes the refresh
    interval from every minute to the minimum of either the next weather
    refresh or the next event start/end.
    
    It also now detects more events (new calendar entry, deleted calendar
    entry, location change, timezone change etc)
    
    Patch set 7 : Change the startup of the provider to only update the
                  required panels on enabling the appwigdet and updates
                  This further reduces the number of times the widget
                  refreshes itself, now limited only to actual events
    Patch set 8 : Change the weather refresh time to be absolute, not
                  relative to current time
    Patch set 9 : Refactor the calendar to use a CalendarInfo class that
                  maintains a static list of events.  This allows the
                  refreshing of the lock screen widget without querying
                  the calendar provider every time the screen turns on
    
    Change-Id: I0496dad356c92fb26ad7289268327b27b365b6cd

--- src/com/cyanogenmod/lockclock/ClockWidgetProvider.java
+++ src/com/cyanogenmod/lockclock/ClockWidgetProvider.java
@@ -22,37 +22,116 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.cyanogenmod.lockclock.misc.Constants;
 
 public class ClockWidgetProvider extends AppWidgetProvider {
     private static final String TAG = "ClockWidgetProvider";
+    private static boolean DEBUG = true;
+
+    // We always start with the clock as true, the rest we set later
+    private static boolean mRefreshClock = true;
+    private static boolean mRefreshWeather = false;
+    private static boolean mRefreshCalendar = false;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        updateWidgets(context, null);
+        updateWidgets(context);
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
-        updateWidgets(context, intent);
-    }
 
-    private void updateWidgets(Context context, Intent intent) {
-        // Update the widget via the service. Build the intent to call the service on a timer
-        Intent i = new Intent(context.getApplicationContext(), ClockWidgetService.class);
-        PendingIntent pi = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+        if (DEBUG)
+            Log.d(TAG, "onReceive: " + intent.toString());
 
-        // See if we are forcing a full refresh and trigger a single update
-        if (intent != null && intent.getBooleanExtra(Constants.FORCE_REFRESH, false)) {
-            i.putExtra(Constants.FORCE_REFRESH, true);
-            context.startService(i);
+        // Deal with received broadcasts that force a refresh
+        String action = intent.getAction() != null ? intent.getAction() : "";
+        if (DEBUG)
+            Log.d(TAG, "onReceive: Action = " + action);
+
+        // Initially, when the widget is ENABLED we only show the clock so only update it
+        // UPDATE_OPTIONS gets called for a lock screen widget every time the screen turns on
+        // but only for a home screen widget on resize. For now, don't do anything
+        // special other than updating the clock since the calendar and weather will
+        // refresh as needed
+        if (action.equals("android.appwidget.action.APPWIDGET_ENABLED")
+                || action.equals("android.appwidget.action.APPWIDGET_UPDATE_OPTIONS")) {
+            mRefreshClock = true;
+
+        // Calendar, Time or a settings change, force a calendar refresh
+        } else if (action.equals(Intent.ACTION_PROVIDER_CHANGED)
+                || action.equals(Intent.ACTION_TIME_CHANGED)
+                || action.equals(Intent.ACTION_TIMEZONE_CHANGED)
+                || action.equals(Intent.ACTION_DATE_CHANGED)
+                || action.equals(Intent.ACTION_LOCALE_CHANGED)
+                || intent.getBooleanExtra(Constants.REFRESH_CALENDAR, false)) {
+            mRefreshCalendar = true;
+
+        // Weather settings change, force a weather refresh
+        // TODO: This should also include a location listener
+        } else if (intent.getBooleanExtra(Constants.REFRESH_WEATHER, false)) {
+            mRefreshWeather = true;
+
+        // Clock settings change, force a clock refresh
+        } else if (intent.getBooleanExtra(Constants.REFRESH_CLOCK, false)) {
+            mRefreshClock = true;
+
+        // We are not forcing a refresh, normal handling
+        } else {
+            mRefreshClock = false;
+            mRefreshCalendar = false;
+            mRefreshWeather = false;
+            super.onReceive(context, intent);
         }
 
-        // Clear any old alarms and schedule the new alarm that only triggers if the device is ON (RTC)
+        if (DEBUG) {
+            Log.d(TAG, "Refreshing clock = " + mRefreshClock
+                    + ", weather = " + mRefreshWeather
+                    + " and Calendar = " + mRefreshCalendar);
+        }
+
+        // Now call the update
+        updateWidgets(context);
+    }
+
+    /**
+     *  Update the widget via the service.
+     * @param context
+     */
+    private void updateWidgets(Context context) {
+        // Build the intent to call the service
+        Intent i = new Intent(context.getApplicationContext(), ClockWidgetService.class);
+
+        // See if we are forcing a refresh of the clock - its done via settings change
+        if (mRefreshClock) {
+            i.putExtra(Constants.REFRESH_CLOCK, true);
+            mRefreshClock = false;
+        }
+
+        // See if we are forcing a refresh of weather - its done via settings and user click
+        if (mRefreshWeather) {
+            i.putExtra(Constants.REFRESH_WEATHER, true);
+            mRefreshWeather = false;
+        }
+
+        // See if we are forcing a refresh of the calendar - its done via settings change
+        if (mRefreshCalendar) {
+            i.putExtra(Constants.REFRESH_CALENDAR, true);
+            mRefreshCalendar = false;
+        }
+
+        // Start the service. The service itself will take care of scheduling refreshes if needed
+        context.startService(i);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Unsubscribe from all AlarmManager updates if any exist
+        Intent i = new Intent(context.getApplicationContext(), ClockWidgetService.class);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         am.cancel(pi);
-        am.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), 60000, pi);
     }
 }
