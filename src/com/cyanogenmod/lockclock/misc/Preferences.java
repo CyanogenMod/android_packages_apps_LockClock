768ee29c8ba677f2089649d25f1466de61c2e9f6
Rescue9 <Rescue@corridor9.net>
2013-08-09 13:00:34 -0500

    LockClock: Fix lookahead event display
    Found out today that events starting at 00:00 the next day were
    showing in the calendar when a lookahead time of 'Today' was selected.
    The closer the milliseconds were set to 999 (990 for ex.) caused the
    widget to attempt to fetch the 00:00 event continuously. A value of
    500 provides a safe buffer to prevent continuous event fetching.
    
    Patch 1: Initial patch
    
    Change-Id: I6a0d0795e8970f1491ef198558384db65d639d1e

--- src/com/cyanogenmod/lockclock/misc/Preferences.java
+++ src/com/cyanogenmod/lockclock/misc/Preferences.java
@@ -203,7 +203,7 @@ public class Preferences {
             cal.set(Calendar.HOUR_OF_DAY, 23);
             cal.set(Calendar.MINUTE, 59);
             cal.set(Calendar.SECOND, 59);
-            cal.set(Calendar.MILLISECOND, 999);
+            cal.set(Calendar.MILLISECOND, 500);
             long endtimeToday = cal.getTimeInMillis();
 
             lookAheadTime = endtimeToday - now;
