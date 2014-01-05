1174e6835129e62fc208b2bd6a29c06aaa17e922
Danny Baumann <dannybaumann@web.de>
2014-01-05 14:13:16 +0100

    Port over improvements from Chronus
    - new weather source: OpenWeatherMap
    - weather icon pack support
    - weather forecast activity and popup
    - updated weather and in-app icons
    - some new translations (AR, TR, SL)
    
    Change-Id: I2bcc2042bf83d0e0bb4a00200de1310042303e9c

--- src/com/cyanogenmod/lockclock/ClockWidgetProvider.java
+++ src/com/cyanogenmod/lockclock/ClockWidgetProvider.java
@@ -65,7 +65,7 @@ public class ClockWidgetProvider extends AppWidgetProvider {
         } else if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
             // On first boot lastUpdate will be 0 thus no need to force an update
             // Subsequent boots will use cached data
-            WeatherUpdateService.scheduleNextUpdate(context);
+            WeatherUpdateService.scheduleNextUpdate(context, false);
 
         // A widget has been deleted, prevent our handling and ask the super class handle it
         } else if (AppWidgetManager.ACTION_APPWIDGET_DELETED.equals(action)
@@ -117,7 +117,7 @@ public class ClockWidgetProvider extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         if (D) Log.d(TAG, "Scheduling next weather update");
-        WeatherUpdateService.scheduleNextUpdate(context);
+        WeatherUpdateService.scheduleNextUpdate(context, true);
 
         // Start the broadcast receiver (API 16 devices)
         // This will schedule a repeating alarm every minute to handle the clock refresh
