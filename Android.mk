6cb19162b39b765da56f6bb36d8c99cdb2fee1e4
Valerio Pilo <vpilo@coldshock.net>
2014-04-15 11:48:39 +0200

    goldfish: fix emulator build.
    Build stops with autogenerated files which should be ignored.
    
    Change-Id: Ied7c7bfecffba11be2ae8129b96ec94fd7ceb829

--- Android.mk
+++ Android.mk
@@ -17,7 +17,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_SDK_VERSION := 19
 LOCAL_PACKAGE_NAME := LockClock
