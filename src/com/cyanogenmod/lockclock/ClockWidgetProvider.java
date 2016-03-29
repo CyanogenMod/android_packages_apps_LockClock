ec91890a0983867f37fa3f4b46f8966e886f07f2
Luis Vidal <lvidal@cyngn.com>
2016-03-29 14:37:07 -0700

    Refactor cLock to use the new Weather API
    - Content provider was removed
    - Weather sources (Yahoo and OpenWeatherMap) were removed. Those
      providers will reside in their own package.
    - cLock was refactored to use the Weather API to process weather
      updates and city name disambiguation
    - cLock will let the active weather service provider decide
      whether back to back requests are OK. In other words, cLock
      does not enforce the 10 min wait period between requests anymore
    
    TICKET: CYNGNOS-2116
    
    Change-Id: I87ccd5727013dbbd7e96ce0a8e3d4f766f823072

--- src/com/cyanogenmod/lockclock/ClockWidgetProvider.java
+++ src/com/cyanogenmod/lockclock/ClockWidgetProvider.java
@@ -24,11 +24,11 @@ import android.net.ConnectivityManager;
 import android.util.Log;
 
 import com.cyanogenmod.lockclock.misc.Constants;
+import com.cyanogenmod.lockclock.misc.Preferences;
 import com.cyanogenmod.lockclock.misc.WidgetUtils;
 import com.cyanogenmod.lockclock.weather.ForecastActivity;
+import com.cyanogenmod.lockclock.weather.WeatherSourceListenerService;
 import com.cyanogenmod.lockclock.weather.WeatherUpdateService;
-import com.cyanogenmod.lockclock.ClockWidgetService;
-import com.cyanogenmod.lockclock.WidgetApplication;
 
 public class ClockWidgetProvider extends AppWidgetProvider {
     private static final String TAG = "ClockWidgetProvider";
@@ -64,8 +64,9 @@ public class ClockWidgetProvider extends AppWidgetProvider {
 
         // Boot completed, schedule next weather update
         } else if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
-            // On first boot lastUpdate will be 0 thus no need to force an update
-            // Subsequent boots will use cached data
+            //Since we're using elapsed time since boot, we can't use the timestamp from the
+            //previous boot so we need to reset the timer
+            Preferences.setLastWeatherUpadteTimestamp(context, 0);
             WeatherUpdateService.scheduleNextUpdate(context, false);
 
         // A widget has been deleted, prevent our handling and ask the super class handle it
@@ -124,6 +125,7 @@ public class ClockWidgetProvider extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         if (D) Log.d(TAG, "Scheduling next weather update");
+        context.startService(new Intent(context, WeatherSourceListenerService.class));
         WeatherUpdateService.scheduleNextUpdate(context, true);
 
         // Start the broadcast receiver (API 16 devices)
@@ -138,6 +140,7 @@ public class ClockWidgetProvider extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         if (D) Log.d(TAG, "Cleaning up: Clearing all pending alarms");
+        context.stopService(new Intent(context, WeatherSourceListenerService.class));
         ClockWidgetService.cancelUpdates(context);
         WeatherUpdateService.cancelUpdates(context);
 
